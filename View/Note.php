be6568d5ddc83f432f4a9f25a412064486f8b41b
Marius <mariusfranck20@gmail.com>
2023-03-24 14:22:43 +0100

    All

---
+++ View/Note.php
@@ -0,0 +1,18 @@
+<?php ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="../View/StyleSheets/StyleGénéral.css">
+    <link rel="stylesheet" href="../View/StyleSheets/Bienvenue.css">
+</head>
+<header>
+    <img class="Logo_Boulanger" src="Images/Boulanger_Long.png" alt="images">
+</header>
+
+<body>
+</body>
+
+</html>
+
+
